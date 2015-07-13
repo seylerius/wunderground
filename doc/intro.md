2bbed1533d29f04ebe6ed12ae9aa79c2cc17483f
Elliott Seyler <emhs08@gmail.com>
2015-07-13 15:16:40 -0700

    Initial commit with lein setup

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wunderground
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/com/seriouslyseylerius/wunderground_test.clj
@@ -0,0 +1,7 @@
+(ns com.seriouslyseylerius.wunderground-test
+  (:require [clojure.test :refer :all]
+            [com.seriouslyseylerius.wunderground :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
